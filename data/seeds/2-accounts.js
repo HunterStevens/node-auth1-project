266c3b38ff0b19a6481f6898287c741a5900a29c
Hunter Stevens <hunterstevens1996@gmail.com>
2020-05-18 15:41:00 -0600

    fixing some syntax errors

--- data/seeds/2-accounts.js
+++ data/seeds/2-accounts.js
@@ -4,17 +4,17 @@ exports.seed = function(knex) {
     {
       username: "groot",
       password: "Iamgroot!",
-      role: 1,
+      profile_id: 1,
     },
     {
       username: "admin",
       password: "keepitsecret,keepitsafe.",
-      role: 1,
+      profile_id: 1,
     },
     {
       username: "me",
       password: "changethepass",
-      role: 2,
+      profile_id: 2,
     },
     {
       username: "nobody",
@@ -24,7 +24,7 @@ exports.seed = function(knex) {
       username: "notme",
       password: "hasnorole",
     }
-  ]
+  ];
 
   return knex('accounts').insert(users)
 };
